1cd87cbfa79a286d3e29d02c9a134d9c3404e2a9
Chad Fanguy <cfanguy@gmail.com>
2019-03-18 23:15:33 -0400

    Added location file
    TODO: Figure out way to open txt file from compiled jar

--- src/main/java/com/cfanguy/Main.java
+++ src/main/java/com/cfanguy/Main.java
@@ -1,5 +1,8 @@
 package com.cfanguy;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.stereotype.Controller;
@@ -23,7 +26,26 @@ public class Main {
   @RequestMapping(value="/data", method=RequestMethod.GET)
   @ResponseBody
   public String getData() {
-    return "Data to Send";
+    // x, y, event, location
+    StringBuilder builder = new StringBuilder();
+    try
+    {
+      builder.append("test");
+
+      // TODO: Figure out way to open file when compiled to .jar
+      BufferedReader reader = new BufferedReader(new FileReader("locations.txt"));
+      String line;
+      while ((line = reader.readLine()) != null)
+      {
+        builder.append(line);
+      }
+      reader.close();
+      return builder.toString();
+    }
+    catch (Exception e)
+    {
+      return builder + e.toString();//"Error occurred reading file";
+    }
   }
 
 }
